0c8190f10a92a70823642d0e21ed6164560b4c6f
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-01-27 14:28:22 +0000

    Created index.html, script.js. style.css
    Pseudocode planning added to script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+function getComputerChoice() {
+    // Randomly returns either "rock", "paper" or "scissors"
+}
+
+function getPlayerChoice() {
+    // Prompt user for 3 possible choices (rock, paper or scissors)
+    // Convert choice to lowercase
+    // Prompt again if choice is invalid
+}
+
+function playRound(playerSelection, computerSelection) {
+    // Compare playerSelection to computerSelection
+
+    // if playerSelection is the same as computerSelection
+        // return "tie"
+
+    // if playerSelection is rock
+        // if computerSelection is scissors
+            // return "win"
+        // else return "lose"
+
+    // if playerSelection is scissors
+        // if computerSelection is paper
+            // return "win"
+        // else return "lose"
+
+    // if playerSelection is paper
+        // if computerSelection is rock
+            // return "win"
+        // else return "lose"
+}
+
+function game() {
+    // Loop playRound 5 times
+    // Use counters to track player scores
+    // Return winner with highest score when loop terminates
+}
