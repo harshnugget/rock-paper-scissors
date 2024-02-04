1826dc88c4e3efbeb309b05c9968c383a5448594
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-02-04 01:37:33 +0000

    Added button functionality
    - Added functional buttons that allow a user to choose an option

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+// Add event listener to div containing choice buttons
+// Track which button is clicked and save as choice
+let playerChoice;
+
+document.querySelector("#choices").addEventListener("click", function(event) {
+    switch (event.target.id) {
+    case "rock-btn":
+        playerChoice = "rock";
+        break;
+    case "paper-btn":
+        playerChoice = "paper";
+        break;
+    case "scissors-btn":
+        playerChoice = "scissors";
+        break;
+    };
+});
+
 // Create dictionary that determines winning choices (key beats value)
 const winnerLoserDict = {rock: "scissors", scissors: "paper", paper: "rock"};
 
 function getComputerChoice() {
     // Randomly returns either "rock", "paper" or "scissors"
-
     // Get random number between 1 and 3
     let randomNum = Math.floor(Math.random() * 3) + 1;
     
@@ -16,25 +33,6 @@ function getComputerChoice() {
         }
         index++;
     }
-}
-
-function getPlayerChoice() {
-    // Prompt user for 3 possible choices (rock, paper or scissors)
-    // Continue to prompt if choice not in dictionary (invalid choice)
-    let playerChoice;
-    for (let i = 0; !(playerChoice in winnerLoserDict); i++) {
-        if (i > 0) {
-            console.log("Invalid choice!");
-        }
-        playerChoice = prompt("Rock, Paper or Scissors?");
-
-        // Randomly assign player choice if option is null (prompt is cancelled)
-        if (playerChoice === null) {
-            playerChoice = getComputerChoice();
-            break;
-        }
-    }
-    return playerChoice.toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -52,32 +50,43 @@ function playRound(playerSelection, computerSelection) {
     return "lose"
 }
 
+// Variables for tracking scores
+let playerScore = 0;
+let computerScore = 0;
+
 function game() {
     let result;
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    // Replay rounds until a player reaches a score of 5
-    while (playerScore < 5 && computerScore < 5) {
-        result = playRound(getPlayerChoice(), getComputerChoice());
-        switch (result) {
-            case "win":
-                console.log("You won this round!");
-                playerScore++;
-                break;
-            case "lose":
-                console.log("You lose this round!");
-                computerScore++;
-                break;
-            case "tie":
-                console.log("It's a tie this round!");
-        }
-        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}\n\n`);
-    }
 
-    if (playerScore > computerScore) {
-        return "YOU WIN!";
-    } else {
-        return "YOU LOSE!";
+    // Replay rounds until a player reaches a score of 5
+    if (!(playerChoice)) {
+        console.log("No choice selected");
+        return;
+    };
+    result = playRound(playerChoice, getComputerChoice());
+    switch (result) {
+        case "win":
+            console.log("You won this round!");
+            playerScore++;
+            break;
+        case "lose":
+            console.log("You lose this round!");
+            computerScore++;
+            break;
+        case "tie":
+            console.log("It's a tie this round!");
+    }
+    playerChoice = "";  // Reset player choice after every round
+    console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}\n\n`);
+
+    if (playerScore == 5) {
+        playerScore = 0;
+        computerScore = 0;
+        
+        return "YOU WIN!\n\n";
+    } 
+    if (computerScore == 5) {
+        playerScore = 0;
+        computerScore = 0;
+        return "YOU LOSE!\n\n";
     }
 }
